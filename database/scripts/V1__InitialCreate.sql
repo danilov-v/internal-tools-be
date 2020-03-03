8bd60b8ae2e0b2f0099511a54a1c3daf433a4c9b
Gamezo Vladislav <gamezo@rit.va>
2020-03-03 17:59:51 +0300

    [IT-9] Увеличены размеры строковых полей. Добавлена таблица, хранящая тип пользователя.

--- database/scripts/V1__InitialCreate.sql
+++ database/scripts/V1__InitialCreate.sql
@@ -4,33 +4,38 @@ create schema it_tools;
 
 create table it_tools.role (
     id   serial primary key,
-    name varchar(20) unique not null
+    name varchar(1044) unique not null
 );
 
 create table it_tools.promotion_type (
     id   serial primary key,
-    name varchar(20) unique not null
+    name varchar(255) unique not null
 );
 
 create table it_tools.penalty_type (
     id   serial primary key,
-    name varchar(20) unique not null
+    name varchar(255) unique not null
 );
 
 create table it_tools.removal_reason (
     id   serial primary key,
-    name varchar(20) unique not null
+    name varchar(255) unique not null
 );
 
 create table it_tools.unit_type (
     id   serial primary key,
-    name varchar(20) unique not null
+    name varchar(255) unique not null
 );
 
 create table it_tools.rank (
     id     serial primary key,
-    name   varchar(20) unique not null,
-    rating integer unique     not null
+    name   varchar(255) unique not null,
+    rating integer unique      not null
+);
+
+create table it_tools.person_type (
+    id   serial primary key,
+    name varchar(255) unique not null
 );
 
 -- general tables
@@ -53,38 +58,37 @@ create table it_tools.unit (
     parent_unit  integer references it_tools.unit (id)
 );
 
-create table it_tools.employee (
+create table it_tools.person (
     id                  serial primary key,
-    first_name          varchar(20) not null,
-    second_name         varchar(20) not null,
-    last_name           varchar(20) not null,
+    first_name          varchar(25) not null,
+    second_name         varchar(25),
+    last_name           varchar(25) not null,
     birth_date          date,
-    phone               varchar(20),
-    position            varchar(20),
-    conscript           boolean     not null default true,
+    phone               varchar(25),
+    position            varchar(255),
+    person_type_id      integer references it_tools.person_type (id),
     call_date           date,
     demobilization_date date,
     created_date        date,
-    deleted             boolean     not null default false,
-    comment             varchar(255),
+    comment             text,
     unit_id             integer references it_tools.unit (id),
     rank_id             integer references it_tools.rank (id)
 );
 
-create table it_tools.employee_removal (
+create table it_tools.person_removal (
     id                serial primary key,
-    employee_id       integer references it_tools.employee (id),
+    person_id         integer references it_tools.person (id),
     removal_reason_id integer references it_tools.removal_reason (id),
     removal_date      date not null,
     comment           varchar(255)
-                                       );
+);
 
 create table it_tools.promotion (
     id               serial primary key,
     assignment_date  date not null,
     realization_date date,
     comment          varchar(255),
-    employee_id      integer references it_tools.employee (id),
+    person_id        integer references it_tools.person (id),
     type_id          integer references it_tools.promotion_type (id)
 );
 
@@ -93,6 +97,6 @@ create table it_tools.penalty (
     assignment_date  date not null,
     realization_date date,
     comment          varchar(255),
-    employee_id      integer references it_tools.employee (id),
+    person_id        integer references it_tools.person (id),
     type_id          integer references it_tools.penalty_type (id)
 );
