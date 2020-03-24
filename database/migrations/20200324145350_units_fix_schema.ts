111043d31c5f8f5bcee49e44141415734bba37e5
Gamezo Vladislav <gamezo@rit.va>
2020-03-24 15:29:31 +0300

    Добавлена уникальность на группу [name, parent_unit].

--- database/migrations/20200324145350_units_fix_schema.ts
+++ database/migrations/20200324145350_units_fix_schema.ts
@@ -3,11 +3,13 @@ import * as Knex from "knex";
 export async function up(knex: Knex): Promise<any> {
     return knex.schema.alterTable('units', builder => {
         builder.dropUnique(['name']);
-    });
+        builder.unique(['name', 'parent_unit']);
+    })
 }
 
 export async function down(knex: Knex): Promise<any> {
     return knex.schema.alterTable('units', builder => {
+        builder.dropUnique(['name', 'parent_unit']);
         builder.unique(['name']);
     });
 }
