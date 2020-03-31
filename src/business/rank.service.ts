43b9d546530500616f48422656e3095a54edbc29
Gamezo Vladislav <gamezo@rit.va>
2020-03-31 11:02:05 +0300

    [IT-46]. Добавлена возможность получать звания по id.

--- src/business/rank.service.ts
+++ src/business/rank.service.ts
@@ -3,6 +3,10 @@ import Rank from '../data/rank';
 const rankService = {
     async getAllRanks(): Promise<Rank[]> {
         return Rank.query();
+    },
+
+    async getRankById(rankId: number): Promise<Rank> {
+        return (await Rank.query().findById(rankId)) || {} as Rank;
     }
 };
 
