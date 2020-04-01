80138af64b61003cd0dc2f7c2987500e9f72bf38
Сергей Сивченко <sivchenko@rit.va>
2020-04-01 13:22:31 +0300

    fix(eslint): fix eslint error

--- src/routes/rank.ts
+++ src/routes/rank.ts
@@ -32,7 +32,7 @@ rankRouter.get('/rank', async (req, res) => {
 
 rankRouter.get('/rank/:rankId', async (req, res) => {
     try {
-        let rankId = Number.parseInt(req.params.rankId);
+        const rankId = Number.parseInt(req.params.rankId);
         const ranks = await rankService.getRankById(rankId);
 
         res.json(plainToClass(RankInfoDto, ranks));
